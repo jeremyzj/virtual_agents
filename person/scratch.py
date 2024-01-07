f0354ca502ef2d4ea8e258dd64a4818d08897860
ward.zhang@quectel.com <ward.zhang@quectel.com>
2024-01-07 16:45:57 +0800

    Refactor code to improve performance and readability

---
+++ person/scratch.py
@@ -0,0 +1,16 @@
+
+class Scratch: 
+  def __init__(self, f_saved):
+    # THE CORE IDENTITY OF THE PERSONA 
+    # Base information about the persona.
+    self.name = None
+    self.first_name = None
+    self.last_name = None
+    self.age = None
+    # L0 permanent core traits.  
+    self.innate = None
+    # L1 stable traits.
+    self.learned = None
+    # L2 external implementation. 
+    self.currently = None
+    self.lifestyle = None
